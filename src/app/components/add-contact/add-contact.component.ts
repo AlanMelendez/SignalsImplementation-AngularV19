190c26af5e611afda074915f3c5db0af205cdc24
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-10 12:31:11 -0600

    Implement form submission in AddContactComponent and enhance data binding; update ContactsListComponent to display contacts conditionally

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,17 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [CommonModule,MatFormFieldModule, FormsModule, MatButtonModule, MatIconModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    RouterModule,
+  ],
   templateUrl: './add-contact.component.html',
-  styleUrl: './add-contact.component.scss'
+  styleUrl: './add-contact.component.scss',
 })
 export class AddContactComponent {
+  name = signal<string>(''); 
+  email = signal<string>('');
+  phone = signal<string>('');
 
+  addContact() {
+    console.log('Name:', this.name());
+    console.log('Email:', this.email());
+    console.log('Phone:', this.phone());
+  }
 }
