9498e351b7187e9aceb6590caa6a31ceef6224ca
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-10 13:34:05 -0600

    Add PrimeNG and animations support; enhance AddContactComponent with loading spinner and form validation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+            darkModeSelector: false || 'none'
+        }
+      }
+  })
+  ],
 };
+
--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -5,9 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { ApiService } from '../../services/api.service';
-
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-add-contact',
   imports: [
@@ -17,29 +19,57 @@ import { ApiService } from '../../services/api.service';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    RouterModule,
+    MatProgressSpinner,
+    RouterLink,
+    ToastModule,
   ],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.scss',
+  providers: [MessageService]
 })
 export class AddContactComponent {
   apiService = inject(ApiService);
+  router = inject(Router);
+  messageService = inject(MessageService);
 
   name = signal<string>(''); 
   email = signal<string>('');
   phone = signal<string>('');
 
-  async addContact() {
-    console.log('Name:', this.name());
-    console.log('Email:', this.email());
-    console.log('Phone:', this.phone());
+  loadingSave = signal<boolean>(false);
 
+  async addContact() {
+    if(!this.validateForm()) {
+      return;
+    }
+
+    this.loadingSave.set(true);
     await this.apiService.addContact({
       id: Math.random(),
       name: this.name(),
       email: this.email(),
       phone: this.phone(),
     });
+    this.messageService.add({severity: 'success', summary: 'Success', detail: 'Contact added successfully'});
+    setTimeout(() => {
+      this.loadingSave.set(false);
+    this.router.navigate(['/']);
+    }, 1000);
+  }
+
+
+  validateForm() {
+    //Anyone of the fields not must been empty to enable the save button
+    
+    if(this.name() === '' || this.email() === '' || this.phone() === '') {
+      this.messageService.add({severity: 'error', summary: 'Error', detail: 'All fields are required'});
+            return false;
+    }
+
+    return true;
+
 
   }
+
+
 }
