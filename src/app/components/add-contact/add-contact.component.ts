07e3e9529bec8fa005b5bacb5f22092aca82f7b2
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-10 12:17:22 -0600

    Add AddContactComponent with form fields and styling; update app component layout

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [],
+  imports: [CommonModule,MatFormFieldModule, FormsModule, MatButtonModule, MatIconModule, MatInputModule],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.scss'
 })
