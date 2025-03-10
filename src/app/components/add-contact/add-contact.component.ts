17e41f2936e9e36a6d3429d4e6e693ad5c6152b4
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-10 12:35:39 -0600

    Refactor AddContactComponent to use experimental zoneless change detection; integrate ApiService for adding contacts

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink, RouterModule } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-add-contact',
@@ -22,13 +23,23 @@ import { RouterLink, RouterModule } from '@angular/router';
   styleUrl: './add-contact.component.scss',
 })
 export class AddContactComponent {
+  apiService = inject(ApiService);
+
   name = signal<string>(''); 
   email = signal<string>('');
   phone = signal<string>('');
 
-  addContact() {
+  async addContact() {
     console.log('Name:', this.name());
     console.log('Email:', this.email());
     console.log('Phone:', this.phone());
+
+    await this.apiService.addContact({
+      id: Math.random(),
+      name: this.name(),
+      email: this.email(),
+      phone: this.phone(),
+    });
+
   }
 }
